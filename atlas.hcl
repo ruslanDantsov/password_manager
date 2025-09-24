5c819e49d928ef8e29b0d554b9ac882c116c6d5f
Ruslan <rdantsov@tula.co>
2025-09-25 00:44:11 +0300

    clean master

--- atlas.hcl
+++
@@ -1,6 +0,0 @@
-env "local" {
-  url = "postgres://postgres:RedDawn_84@localhost:5432/password_manager?sslmode=disable"
-  migration {
-    dir = "file://server/migrations"
-  }
-}
--- gen.go
+++
@@ -1,5 +0,0 @@
-package password_manager
-
-//go:generate buf generate
-//go:generate atlas migrate apply --env local
-//go:generate sqlc generate -f ./server/repository/sqlc/sqlc.yaml
